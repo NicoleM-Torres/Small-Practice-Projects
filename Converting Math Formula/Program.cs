5aa39454f5c39080fe0db48870db0307f8662133
Nicole Torres <nicole.torres@mscoding.org>
2024-05-20 11:37:04 -0500

    Create Program.cs

---
+++ Converting Math Formula/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Converting_Math_Formula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //VARIABLES
+                double futureValue = 0.0;
+                double annualInterest = 0.0;
+                double years = 0.0;
+                double presentValue = 0.0;
+          
+
+            //INPUT
+            Console.WriteLine("Enter annual interest:");
+                annualInterest = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter years in account:");
+                years = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter accounts expected future value:");
+                futureValue = double.Parse(Console.ReadLine());
+
+
+            //PROCESSES
+                presentValue = (futureValue / Math.Pow((1.0 + annualInterest), years));            
+
+
+            //OUTPUT
+                Console.WriteLine($"Your expected deposit today is:{presentValue}");
+
+
+        } //END MAIN    
+    } //END CLASS
+} //END NAMESPACE
